d32aa7c74c4f4669867e940c8a07eb68576bb6a6
Cristhian Caldas <c.calda.m@gmail.com>
2023-03-25 16:03:46 -0500

    añadiendo test Unitarios

--- src/test/java/com/demo/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/demo/userservice/UserServiceApplicationTests.java
@@ -1,15 +1,20 @@
 package com.demo.userservice;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class UserServiceApplicationTests {
-
+	@Mock
+	private SpringApplicationBuilder springApplicationBuilder;
 	@Test
 	void contextLoads() {
+		Mockito.when(springApplicationBuilder.sources(UserServiceApplication.class)).thenReturn(springApplicationBuilder);
 		assertTrue(true);
 	}
 
